f708691479a89249894c874eb9b4bc85bf1b6da5
Andrew Munsell <andrew@wizardapps.net>
2014-10-20 12:53:13 -0700

    Add Zepto and modernizr

--- js/app.js
+++ js/app.js
@@ -4,8 +4,15 @@ requirejs.config({
 	baseUrl: 'js',
 
 	paths: {
+		zepto: '../bower_components/zepto/zepto',
 		d3: '../bower_components/d3/d3',
 		topojson: '../bower_components/topojson/topojson'
+	},
+
+	shim: {
+		zepto: {
+			exports: '$'
+		}
 	}
 });
 
