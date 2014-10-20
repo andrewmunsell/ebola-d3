c3063c58b0d92506d71a6d20868d904b7db02aae
Andrew Munsell <andrew@wizardapps.net>
2014-10-20 15:11:19 -0700

    Add moment

--- js/app.js
+++ js/app.js
@@ -5,6 +5,7 @@ requirejs.config({
 
 	paths: {
 		zepto: '../bower_components/zepto/zepto',
+		moment: '../bower_components/moment/moment',
 		d3: '../bower_components/d3/d3',
 		topojson: '../bower_components/topojson/topojson'
 	},
