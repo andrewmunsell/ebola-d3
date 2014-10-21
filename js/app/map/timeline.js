5a7d95c27fef3483c5f48cada337564f8eaa1c2e
Andrew Munsell <andrew@wizardapps.net>
2014-10-21 10:47:46 -0700

    Add event markers
    They still need to be responsive

--- js/app/map/timeline.js
+++ js/app/map/timeline.js
@@ -1,6 +1,6 @@
 'use strict';
 
-define(['require', 'd3', 'zepto'], function(require, d3, zepto) {
+define(['require', 'd3', 'zepto', 'moment'], function(require, d3, zepto, moment) {
 	/**
 	 * Constructor for the timeline that accepts the element to use
 	 */
@@ -8,6 +8,11 @@ define(['require', 'd3', 'zepto'], function(require, d3, zepto) {
 		this.el = el;
 		this.width = width;
 		this.height = height;
+
+		this.start = moment().subtract(1, 'year');
+		this.end = moment();
+
+		this.eventData = [];
 	};
 
 	/**
@@ -17,30 +22,99 @@ define(['require', 'd3', 'zepto'], function(require, d3, zepto) {
 	Timeline.prototype.setWidth = function(width) {
 		this.width = width;
 
-		this.resize();
+		this.redraw();
 	};
 
-	Timeline.prototype.resize = function() {
+	/**
+	 * Add an event at the specified time with the specified name and data
+	 */
+	Timeline.prototype.addEvent = function(time, data) {
+		if(data.name.length > 12) {
+			console.log('You may want to shorten the event name.');
+		}
+
+		this.eventData.push({
+			time: time,
+			data: data
+		});
+		this.redraw();
+	};
+
+	/**
+	 * Resize the timeline based on the current width and height
+	 */
+	Timeline.prototype.redraw = function() {
+		var self = this;
+
+		/**
+		 * Backdrop
+		 */
+
 		this.backdrop
 			.attr('x', this.width * 0.15)
 			.attr('y', 0)
 			.attr('width', this.width * 0.7)
 			.attr('height', this.height)
 			.attr('rx', this.height / 2)
-			.attr('ry', this.height / 2)
+			.attr('ry', this.height / 2);
+
+		/**
+		 * Event Markers
+		 */
+
+		var eventMarker = this.eventContainer
+			.selectAll('g')
+				.data(this.eventData)
+				.enter()
+					.append('g');
+
+		eventMarker
+			.append('circle')
+				.attr('cx', function(d) {
+					var ratio = (d.time.unix() - self.start.unix()) / (self.end.unix() - self.start.unix());
+					return (self.width * 0.15) + (self.width * 0.7 * ratio);
+				})
+				.attr('cy', function(d) {
+					return self.height / 2
+				})
+				.attr('r', 24)
+				.attr('class', 'timeline-event-outer');
+
+		eventMarker
+			.append('circle')
+				.attr('cx', function(d) {
+					var ratio = (d.time.unix() - self.start.unix()) / (self.end.unix() - self.start.unix());
+					return (self.width * 0.15) + (self.width * 0.7 * ratio);
+				})
+				.attr('cy', function(d) {
+					return self.height / 2
+				})
+				.attr('r', 20)
+				.attr('class', 'timeline-event-inner');
 	};
 
 	/**
 	 * Load the timeline
 	 */
 	Timeline.prototype.load = function() {
-		var self = this;
+		this.container = this.el.append('g');
 
-		this.backdrop = this.el.append('g')
+		this.backdrop = this.container
 			.append('rect')
 				.attr('class', 'timeline-backdrop');
 
-		this.resize();
+		this.currentMarker = this.container
+			.append('circle')
+				.attr('cx', this.width * 0.15 + this.height / 2)
+				.attr('cy', this.height / 2)
+				.attr('r', 8)
+				.attr('class', 'timeline-current-time');
+
+		this.eventContainer = this.container
+			.append('g')
+				.attr('class', 'timeline-events');
+
+		this.redraw();
 	};
 
 	return Timeline;
