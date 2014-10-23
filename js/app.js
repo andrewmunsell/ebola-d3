762f5e64816352c7c36c5ebde0067d5d3b51a5d0
Andrew Munsell <andrew@wizardapps.net>
2014-10-23 11:36:42 -0700

    Add underscore

--- js/app.js
+++ js/app.js
@@ -8,6 +8,7 @@ requirejs.config({
 		zepto: '../bower_components/zepto/zepto',
 		deepmerge: '../bower_components/deepmerge/index',
 		moment: '../bower_components/moment/moment',
+		underscore: '../bower_components/underscore/underscore',
 		d3: '../bower_components/d3/d3',
 		topojson: '../bower_components/topojson/topojson'
 	},
