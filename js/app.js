e5f3087a388657ea205eff4348f7988118ade60e
Andrew Munsell <andrew@wizardapps.net>
2014-10-23 10:59:06 -0700

    Add merge library

--- js/app.js
+++ js/app.js
@@ -6,6 +6,7 @@ requirejs.config({
 	paths: {
 		q: '../bower_components/q/q',
 		zepto: '../bower_components/zepto/zepto',
+		deepmerge: '../bower_components/deepmerge/index',
 		moment: '../bower_components/moment/moment',
 		d3: '../bower_components/d3/d3',
 		topojson: '../bower_components/topojson/topojson'
