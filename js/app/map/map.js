72f7a5d1a68ad6a05ea435e66ff7269eab16314f
Andrew Munsell <andrew@wizardapps.net>
2014-10-28 11:48:45 -0700

    Initial point plotting

--- js/app/map/map.js
+++ js/app/map/map.js
@@ -1,6 +1,6 @@
 'use strict';
 
-define(['require', 'zepto', 'd3', 'topojson'], function(require, $, d3, topojson) {
+define(['require', 'zepto', 'moment', 'd3', 'topojson', '../data/Locator'], function(require, $, moment, d3, topojson, Locator) {
 	/**
 	 * Path to the TopoJSON file to use for the map
 	 * @type {String}
@@ -12,6 +12,12 @@ define(['require', 'zepto', 'd3', 'topojson'], function(require, $, d3, topojson
 	 * @type {Number}
 	 */
 	var maxLatitude = 83;
+
+	/**
+	 * Locator instance for the map
+	 * @type {Locator}
+	 */
+	var locator = new Locator();
 
 	/**
 	 * Initialize a new map with the specified element. 
@@ -113,6 +119,8 @@ define(['require', 'zepto', 'd3', 'topojson'], function(require, $, d3, topojson
 
 		this.el.selectAll('path') 
 			.attr('d', this.path);
+
+		this.plotPoints();
 	};
 
 	/**
@@ -146,6 +154,142 @@ define(['require', 'zepto', 'd3', 'topojson'], function(require, $, d3, topojson
 
 			self.redraw.call(self);
 		});
+
+		require(['../data/Collector'], function(Collector) {
+			var collector = new Collector();
+			collector.collect(function(data) {
+				self.initPoints.call(self, data);
+			});
+		});
+	};
+
+	/**
+	 * Initialize the points on the map from the specified dataset
+	 * @param  {object} data Data to plot
+	 */
+	Map.prototype.initPoints = function(data) {
+		var arrayData = this.processPoints(data);
+
+		this.el.selectAll('circle')
+			.data(arrayData)
+			.enter()
+				.append('circle')
+					.attr('r', 5)
+					.attr('class', function(d) {
+						return 'ping ' + d.code;
+					});
+
+		this.plotPoints(arrayData);
+	};
+
+	/**
+	 * Process the object containing the data for the points with keys as the date and
+	 * values as the countries and subpoint data, transforming it into an array of data points
+	 * with one array item per location.
+	 *
+	 * Each country will have a set of aggregate statistics, and if available, each state/subregion
+	 * and city will have its own data point that will be used at higher zoom levels.
+	 *
+	 * Yes, this is an absolute mess of nested for loops. I could use recursion, but that's for the
+	 * future if this is too unweildly.
+	 * 
+	 * @param  {object} data Object containing the data
+	 * @return {array}       Array of data points, with one point per country and subregion/city
+	 */
+	Map.prototype.processPoints = function(data) {
+		// Convert the date based data format into a country based format so that
+		// we can perform lookups quickly (i.e. it's a hash table, versus looping
+		// over things repeatedly).
+		var processedData = {};
+
+		var putData = function(code, type, date, data) {
+			if(processedData.hasOwnProperty(code)) {
+				// If the data for the current date already exists on the specified point,
+				// we can just overwrite it.
+				processedData[code]['data'][date] = data;
+			} else {
+				processedData[code] = {
+					type: type,
+					data: {}
+				};
+
+				processedData[code]['data'][date] = data;
+			}
+		};
+
+		// Process the data for each country, region, and city in the nightmare of
+		// nested loops.
+		for(var d in data) {
+			if(data.hasOwnProperty(d)) {
+				var date = data[d];
+
+				for(var c in date) {
+					if(date.hasOwnProperty(c)) {
+						var country = date[c];
+
+						for(var r in country) {
+							if(country.hasOwnProperty(r)) {
+								var region = country[r];
+
+								for(var city in region) {
+									if(region.hasOwnProperty(city)) {
+										var point = region[city];
+
+										// Check and see if this is the country level data
+										if(r == 'ZZ' && city.toLowerCase() == 'unknown') {
+											putData(c, 'country', d, point);
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+
+		var arrayData = [];
+
+		// Now, convert the object to an array so that D3 can use it, and pre-lookup the 
+		// latitudes and longitudes to avoid during during each render cycle.
+		for(var i in processedData) {
+			if(processedData.hasOwnProperty(i)) {
+				var code = i.split('.');
+				var coordinates = locator.locate(code[0], code.length >= 2 ? code[1] : null, code.length == 3 ? code[2] : null);
+
+				arrayData.push({
+					code: i,
+					type: processedData[i].type,
+					data: processedData[i].data,
+					coordinates: coordinates
+				});
+			}
+		}
+
+		return arrayData;
+	};
+
+	/**
+	 * Plot all of the points on the map and project them from their
+	 * latitude and longitude
+	 * @param  {object} data Data to plot
+	 */
+	Map.prototype.plotPoints = function(data) {
+		var self = this;
+
+		this.el.selectAll('circle')
+			.attr('transform', function(d) {
+				if(d.coordinates == null) {
+					return null;
+				}
+
+				var c = self.projection([d.coordinates.longitude, d.coordinates.latitude]);
+				if(isNaN(c[0]) || isNaN(c[1])) {
+					return null;
+				}
+				
+				return 'translate(' + c + ')';
+			})
 	};
 
 	return Map;
